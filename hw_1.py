b8e76fe3f721e116b02931e8efe052011644dead
Кирилл Шумаков <kirill.shumakov@gmail.com>
2021-09-07 16:15:31 +0300

    Add hm

---
+++ hw_1.py
@@ -0,0 +1,59 @@
+# 1) Variable is str
+s = "Kirill"
+print(s, type(s))
+
+# 2) Variable is int
+i = 1
+print(i, type(i))
+
+# 3) Variable is float
+f = 2.4
+print(f, type(f))
+
+# 4) Variable is bytes !!!!!!!!!!!!!!!!!!!!!
+
+# 5) Variable is list
+l = [1, 2.2, "Hello"]
+print(l, type(l))
+
+# 6) Variable is tuple
+t = (1, 2.0, "hello")
+print(t, type(t))
+
+# 7) Variable is set
+m = {"a", "b"}
+print(m, type(m))
+
+# 8) Variable is frozen set !!!!!!!!!!!!!!!!
+
+# 9) Variable is dict
+d = {"Ivan": "+1-23-45", "Ann": "+1-23-45"}
+print(d, type(d))
+
+
+# 10)
+he = "Hello "
+wo = "World"
+end = he + wo
+print(end)
+
+# 11)
+r = 10
+u = 3
+
+o = r + u
+print(o)
+
+qq = r / u
+print(qq)
+
+ww = r * u
+print(ww)
+
+ee = r // u
+print(ee)
+
+uu = r % u
+print(uu)
+
+
